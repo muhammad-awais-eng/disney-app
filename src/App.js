fd0c15588fb9ca85c921c824026e08a0dc4e0e7e
wajahatkayani <wajahatkayani042@gmail.com>
2021-04-21 11:26:19 +0500

    header change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">header</div>;
+  return <div className="App">Phar lawan ty tera</div>;
 }
 
 export default App;
